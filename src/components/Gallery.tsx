8b5710cb1f0885b691fc1852105c58b85362886b
Pablo Oyarce <pablooyarceramirez@gmail.com>
2024-06-02 19:05:34 -0400

    chore: Update header chip text and add footer with contact links

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { RefreshCw } from 'lucide-react';
+
+export function Gallery() {
+  const [principalImg, setPrincipalImg] = useState<number>(0);
+
+  const pictures = [
+    "./src/img/perfil1.avif",
+    "./src/img/perfil2.avif",
+    "./src/img/perfil3.avif"
+  ]
+
+  const handleClick = () => {
+    setPrincipalImg(principalImg + 1);
+
+    if (principalImg === pictures.length - 1) {
+      setPrincipalImg(0);
+    }
+
+  }
+
+  return (
+    <div className="relative">
+      <button
+        onClick={handleClick}
+        className="absolute flex items-center gap-2 left-64 top-0 z-20 bg-chip text-white rounded-full py-2 px-4 border-2 border-unst hover:bg-unst hover:text-chip transition-all duration-300 ease-in-out"
+      ><RefreshCw size={18} /> Swop!</button>
+      <img
+        src={pictures[principalImg]}
+        alt="Foto de Pablo Oyarce"
+        className="border-4 border-unst z-10 w-80 h-80 aspect-square object-cover rounded-xl -rotate-2"
+      />
+      <img
+        src={pictures[principalImg + 1] || pictures[0]}
+        alt="Foto de Pablo Oyarce"
+        className="absolute top-4 left-8 rotate-3 -z-10 w-80 h-80 aspect-square object-cover rounded-xl"
+      />
+    </div>
+  )
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,35 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		colors: {
-			'unst': '#16ECD3',
-			'parrafo': '#D1D5DB',
-			'chip': '#161D39',
-			'black': '#000000',
-			'white': '#FFFFFF',
-			'gray': {
-				50: '#F9FAFB',
-				100: '#F3F4F6',
-				200: '#E5E7EB',
-				300: '#D1D5DB',
-				400: '#9CA3AF',
-				500: '#6B7280',
-				600: '#4B5563',
-				700: '#374151',
-				800: '#1F2937',
-				900: '#111827',
-			},
-		},
-		container: {
-			center: true,
-			screens: {
-				sm: '90%',
-				md: '90%',
-				lg: '850px',
-				xl: '850px',
-			}
-		}
-	},
-	extend: {},
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      'unst': '#16ECD3',
+      'parrafo': '#D1D5DB',
+      'chip': '#161D39',
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'gray': {
+        50: '#F9FAFB',
+        100: '#F3F4F6',
+        200: '#E5E7EB',
+        300: '#D1D5DB',
+        400: '#9CA3AF',
+        500: '#6B7280',
+        600: '#4B5563',
+        700: '#374151',
+        800: '#1F2937',
+        900: '#111827',
+      },
+    },
+    container: {
+      center: true,
+      screens: {
+        sm: '90%',
+        md: '90%',
+        lg: '850px',
+        xl: '850px',
+      }
+    }
+  },
+  extend: {},
 };
