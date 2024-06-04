a968f60d56256f415cce2924e41d29a239fcd152
Pablo Oyarce <pablooyarceramirez@gmail.com>
2024-06-03 23:26:21 -0400

    chore: Add dynamic projects to data.ts

---
+++ src/lib/data.ts
@@ -0,0 +1,127 @@
+interface Link {
+  url: string;
+  name: string;
+}
+
+type Device = "Mobile" | "Tablet" | "Desktop" | string;
+
+type Tool = | "HTML5"
+  | "CSS"
+  | "TypeScript"
+  | "React"
+  | "NextJS"
+  | "NodeJS"
+  | "Express"
+  | "TailwindCSS"
+  | "Bootstrap"
+  | "PHP"
+  | "Shopify"
+  | "Amazon Web Services"
+  | "Astro"
+  | "Python"
+  | "PostgreSQL"
+  | "React Query"
+  | "jQuery"
+  | "Wordpress";
+
+interface Project {
+  id: number;
+  title: string;
+  description: string;
+  tools: Tool[];
+  devices: Device[];
+  image: string;
+  links: Link[];
+}
+
+export const PROJECTS: Project[] = [
+  {
+    id: 1,
+    title: "TeaChill - Shopify headless Ecommerce",
+    description: "Ecommerce de venta de variedades de té enfocado en la performance del sitio, desarrollado con NextJS y Shopify como headless CMS",
+    tools: [
+      "TypeScript",
+      "NextJS",
+      "Shopify",
+      "TailwindCSS"
+    ],
+    devices: ["Mobile", "Desktop"],
+    image: "Teachill",
+    links: [
+      {
+        url: "https://github.com/Unstopabloo/Tea-Chill",
+        name: "Github"
+      },
+      {
+        url: "https://tea-chill.vercel.app",
+        name: "Visitar"
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: "Foro MentorU - Foro interactivo con AI",
+    description: "Proyecto de trabajo en el cual se me encargo desarrollar la interfaz y las APIs con NextJS y AWS, este foro esta potenciado con AI para entregar feedback preciso y oportuno a los estudiantes.",
+    tools: [
+      "TypeScript",
+      "NextJS",
+      "React",
+      "NodeJS",
+      "Python",
+      "Amazon Web Services",
+      "React Query",
+      "PostgreSQL"
+    ],
+    devices: ["Mobile", "Tablet", "Desktop"],
+    image: "MentorU",
+    links: [
+      {
+        url: "https://mentoru-forum-dev.vercel.app/",
+        name: "Visitar"
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "KairosX - Administrador de finanzas personales",
+    description: "KairosX actúa como un organizador de gastos personales, que nació con la necesidad propia de organizar mis gastos e ingresos.",
+    tools: [
+      "TypeScript",
+      "NextJS",
+      "PostgreSQL",
+      "TailwindCSS"
+    ],
+    devices: ["Desktop"],
+    image: "Kairos",
+    links: [
+      {
+        url: "https://github.com/Unstopabloo/KairosX",
+        name: "Github"
+      },
+      {
+        url: "https://kairos-x.vercel.app",
+        name: "Visitar"
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: "Spotify Redesign - rediseño en Wordpress",
+    description: "Rediseño de spotify, proyecto encargado como parte de examen. Con un reproductor de musica interactivo integrado, desarrollado en Wordpress con PHP y jQuery.",
+    tools: [
+      "Wordpress",
+      "PHP",
+      "jQuery",
+      "Bootstrap"
+    ],
+    devices: ["Mobile", "Desktop"],
+    image: "Spotify",
+    links: [
+      {
+        url: "https://pabloyarce.laboratoriodiseno.cl/spotify-redesign/",
+        name: "Visitar"
+      }
+    ]
+  },
+
+]
