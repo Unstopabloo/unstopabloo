4cf517dd9973729f09c8f6c4d73320128c6d3a77
Pablo Oyarce <pablooyarceramirez@gmail.com>
2026-02-15 23:23:51 -0300

    prod fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
-import vue from '@astrojs/vue';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://unstopabloo.dev",
-  integrations: [sitemap(), vue(), react()],
+  integrations: [sitemap(), react()],
 
   vite: {
     plugins: [tailwindcss()]
